f9614bed93addbf962b7ffd194bc98437e1a7250
shubham <shubham@alveo.fit>
2022-03-07 18:24:12 +0530

    >>toast message added

--- app/src/main/java/com/shubham/movies/activity/MainActivity.java
+++ app/src/main/java/com/shubham/movies/activity/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.shubham.movies.R;
 import com.shubham.movies.fragment.LatestMoviesFragment;
@@ -73,6 +74,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         moviesDetailsFragment = new MoviesDetailsFragment();
 
         setFragment(latestMoviesFragment);
+
+        Toast.makeText(activity, "Click on movie to get details", Toast.LENGTH_LONG).show();
     }
 
     @Override
